93b7766ac63b17295731ca49be4113dfdb6ca5b6
evan <evanbonner99@gmail.com>
2023-05-09 16:10:28 +0100

    Add cloud variables for buzzer temperature thresholds
    Also removes the date cloud variable, as the Arduino Cloud logs the date anyway. Time is still
    kept track of for printing to serial out.

--- project-semester-2.ino
+++ project-semester-2.ino
@@ -10,18 +10,22 @@
 
 #include "thingProperties.h"
 
+#define SEVENTY_YEARS 2208988800UL
+
 static int serialPrintf(const char *fmt, ...);
-static time_t fetchTime();
+static time_t fetchTime(void);
 
 WiFiUDP udp;
 const uint16_t LOCAL_PORT = 2390;
 
-uint32_t current_time = 0;
-char msg[48] = {0};
+static char msg[48] = {0};
+const auto buzzer_pin = 6;
+const int thermistor_pin = A1;
 
 void
 setup(void) {
 	Serial.begin(9600);
+	pinMode(thermistor_pin, INPUT);
 
 	int status = WL_IDLE_STATUS;
 	while (status != WL_CONNECTED) {
@@ -36,29 +40,37 @@ setup(void) {
 		delay(2000);
 	}
 
-	Serial.println("Connected");
-
 	udp.begin(LOCAL_PORT);
 
 	setSyncProvider(fetchTime);
 	setSyncInterval(3600); // Resync every hour
 	
-	
-  // Defined in thingProperties.h
-  initProperties();
+	// Arduino Cloud
+	initProperties();
+	ArduinoCloud.begin(ArduinoIoTPreferredConnection);
 
-  // Connect to Arduino IoT Cloud
-  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+	tone(buzzer_pin, 512, 500);
 }
 
 void
 loop(void) {
-  ArduinoCloud.update();
-	static char buf[64];
-	const time_t temp = now();
-	const auto time = ctime(&temp);
-	date = time;
-	serialPrintf("%s", time);
+	ArduinoCloud.update();
+
+	static char buf[32];
+	const auto temp = now();
+	strftime(buf, sizeof(buf), "%F %H:%M:%S", gmtime(&temp));
+
+	const float voltage = analogRead(thermistor_pin) * 3.3 / 1024;
+	temperature = (voltage - 0.55) * 100;
+
+	if (temperature >= buzzer_temp_high) {
+		tone(buzzer_pin, 4096, 250);
+	} else if (temperature <= buzzer_temp_low) {
+		tone(buzzer_pin, 512, 250);
+	}
+
+	Serial.print(temperature);
+	serialPrintf(" : %s\n", buf);
 
 	delay(1000);
 }
@@ -67,10 +79,13 @@ void
 pingTimeServer(IPAddress ip) {
 	memset(msg, 0, sizeof(msg));
 
-	msg[0] = 0b11100011;   // LI, Version, Mode
-	msg[1] = 0;     // Stratum, or type of clock
-	msg[2] = 6;     // Polling Interval
-	msg[3] = 0xEC;  // Peer Clock Precision
+	msg[0] |= 0b11000000; // bits 1-2: Set leap second indicator to 3 (unsynchronised)
+	msg[0] |= 0b00100000; // bits 3-5: Set NTP version to 4
+	msg[0] |= 0b00000011; // bits 6-8: Set NTP packet mode to 3 (client)
+
+	msg[1] = 0; // Leave stratum unspecified in client packet
+	msg[2] = 6; // Poll interval
+	msg[3] = 0xEC; // Peer Clock Precision
 
 	Serial.print("Pinging ntp server at ");
 	Serial.println(ip);
@@ -81,19 +96,26 @@ pingTimeServer(IPAddress ip) {
 }
 
 time_t
-fetchTime() {
+fetchTime(void) {
 	IPAddress ip;
 	WiFi.hostByName(NTP_SERVER, ip);
 	pingTimeServer(ip);
 
 	const auto begin = millis();
 	uint32_t packet_size;
-	while (millis() - begin < 2000 && (packet_size = udp.parsePacket()) == 0);
+
+	// Wait either until we get a response or 10 seconds has passed
+	while (millis() - begin < 10000 && (packet_size = udp.parsePacket()) == 0);
+
+	// Got a response
 	if (packet_size > 0 || udp.parsePacket() > 0) {
 		udp.read(msg, sizeof(msg));
-
+		// The received timestamp is big endian - convert to little endian.
 		const uint32_t seconds = msg[40] << 24 | msg[41] << 16 | msg[42] << 8 | msg[43];
-		return seconds - 2208988800UL;
+
+		// NTP returns the number of seconds since January 1 1900. The C99 time functions work with
+		// seconds since the unix epoch (January 1 1970).
+		return seconds - SEVENTY_YEARS;
 	}
 
 	return 0;
@@ -102,7 +124,7 @@ fetchTime() {
 // A wrapper around sprintf and Serial.print. Max buffer size of 256 bytes.
 int
 serialPrintf(const char *fmt, ...) {
-	static char buf[256];
+	static char buf[128];
 	va_list args;
 	va_start(args, fmt);
 
--- thingProperties.h
+++ thingProperties.h
@@ -1,14 +1,21 @@
 #include <ArduinoIoTCloud.h>
 #include <Arduino_ConnectionHandler.h>
 
+static void onBuzzerTempLowChange(void) {}
+static void onBuzzerTempHighChange(void) {}
+
 const char WIFI_SSID[] = SECRET_WIFI_SSID;    // Network SSID (name)
 const char WIFI_PASS[] = SECRET_OPTIONAL_PASS;    // Network password (use for WPA, or use as key for WEP)
 const char NTP_SERVER[] = "ie.pool.ntp.org";
 
-String date;
+CloudTemperatureSensor temperature;
+float buzzer_temp_low = INT_MIN;
+float buzzer_temp_high = INT_MAX;
 
 void initProperties(){
-  ArduinoCloud.addProperty(date, READ, 1 * SECONDS, NULL);
+  ArduinoCloud.addProperty(buzzer_temp_low, READWRITE, ON_CHANGE, onBuzzerTempLowChange);
+  ArduinoCloud.addProperty(buzzer_temp_high, READWRITE, ON_CHANGE, onBuzzerTempHighChange);
+  ArduinoCloud.addProperty(temperature, READ, 5 * SECONDS, NULL);
 }
 
 WiFiConnectionHandler ArduinoIoTPreferredConnection(WIFI_SSID, WIFI_PASS);
