95ced127013741e649fb26b9803bc79d4d146278
evan <evanbonner99@gmail.com>
2023-05-12 05:43:42 +0100

    Init github pages

--- upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-FQBN="arduino:samd:mkr1000"
-PORT="/dev/ttyACM0"
-
-arduino-cli lib install Time ArduinoIoTCloud hd44780
-arduino-cli compile . --fqbn "${FQBN}"
-sudo arduino-cli upload . --fqbn "${FQBN}" --port="${PORT}"
